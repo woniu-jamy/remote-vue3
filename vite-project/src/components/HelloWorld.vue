e7ae68304aa270740c9b0ca56adacbc90d870906
jamy_lin <9851508@qq.com>
2025-04-16 11:18:06 +0800

    这个是vue3项目的第2个版本

--- vite-project/src/components/HelloWorld.vue
+++ vite-project/src/components/HelloWorld.vue
@@ -1,14 +1,18 @@
 <template>
   <div class="card">
     <h2>我是hello组件</h2>
-    <button type="button" @click="count++">count is {{ count }}</button>
+    <button type="button" @click="count1++">count is {{ count1 }}</button>
+    <button type="button" @click="count2++">count is {{ count2 }}</button>
+    <button type="button" @click="count3++">count is {{ count3 }}</button>
   </div>
 </template>
 
 <script setup>
   import { ref } from 'vue'
 
-  const count = ref(0)
+  const count1 = ref(0)
+  const count2 = ref(0)
+  const count3 = ref(0)
 </script>
 
 <style scoped>
