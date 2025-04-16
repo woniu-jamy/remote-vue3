286b8d97325889313635bcaa5bac8dba4f85b256
jamy_lin <9851508@qq.com>
2025-04-16 10:43:29 +0800

    这个是vue3项目的初始版本

---
+++ vite-project/src/App.vue
@@ -0,0 +1,20 @@
+<!-- App.vue就是主页 -->
+<template>
+  <div>
+    <HelloWorld></HelloWorld>
+  </div>
+  <div class="outer1">我是App组件的内容</div>
+</template>
+
+<script setup>
+  import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<style scoped>
+  .outer1 {
+    width: 100px;
+    height: 50px;
+    background-color: blueviolet;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
---
+++ vite-project/src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="card">
+    <h2>我是hello组件</h2>
+    <button type="button" @click="count++">count is {{ count }}</button>
+  </div>
+</template>
+
+<script setup>
+  import { ref } from 'vue'
+
+  const count = ref(0)
+</script>
+
+<style scoped>
+  .card {
+    display: flex;
+    justify-content: center;
+  }
+</style>
